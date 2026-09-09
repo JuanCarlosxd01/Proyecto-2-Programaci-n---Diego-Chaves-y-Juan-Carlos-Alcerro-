3fc608d85bdaf36c0cda01c6daf04513cf73c279
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-09-09 07:15:46 -0600

    Reproductor

--- src/hilos/HiloReproductor.java
+++ src/hilos/HiloReproductor.java
@@ -1,7 +1,52 @@
 
 package hilos;
 
+import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
+import multimedia.ReproductorMusica;
 
-public class HiloReproductor {
+public class HiloReproductor extends Thread {
 
+    private ReproductorMusica reproductor;
+    private JSlider progreso;
+    private boolean activo;
+
+    public HiloReproductor(
+            ReproductorMusica reproductor,
+            JSlider progreso) {
+
+        this.reproductor = reproductor;
+        this.progreso = progreso;
+        this.activo = true;
+    }
+
+    @Override
+    public void run() {
+
+        while (activo) {
+            try {
+                long actual = reproductor.getPosicionActual();
+
+                long duracion = reproductor.getDuracion();
+
+                if (duracion > 0) {
+                    int porcentaje = (int) ((actual * 100) / duracion);
+
+                    SwingUtilities.invokeLater(() -> {
+                        progreso.setValue(porcentaje);
+                    });
+                }
+
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+                activo = false;
+            }
+        }
+    }
+
+    public void detenerHilo() {
+        activo = false;
+        interrupt();
+    }
 }
--- src/multimedia/ReproductorMusica.java
+++ src/multimedia/ReproductorMusica.java
@@ -1,7 +1,106 @@
 
 package multimedia;
 
+import java.io.File;
+import javax.sound.sampled.*;
 
 public class ReproductorMusica {
 
+    private Clip clip;
+    private long posicionPausa;
+    private boolean pausado;
+
+    public ReproductorMusica() {
+        posicionPausa = 0;
+        pausado = false;
+    }
+
+    public void cargarCancion(File archivo) throws Exception {
+
+        detener();
+
+        AudioInputStream audio =
+                AudioSystem.getAudioInputStream(archivo);
+
+        clip = AudioSystem.getClip();
+        clip.open(audio);
+
+        posicionPausa = 0;
+        pausado = false;
+    }
+
+    public void reproducir() {
+
+        if (clip == null) {
+            return;
+        }
+
+        if (pausado) {
+            clip.setMicrosecondPosition(posicionPausa);
+        }
+
+        clip.start();
+        pausado = false;
+    }
+
+    public void pausar() {
+
+        if (clip == null) {
+            return;
+        }
+
+        if (clip.isRunning()) {
+            posicionPausa = clip.getMicrosecondPosition();
+            clip.stop();
+            pausado = true;
+        }
+    }
+
+    public void detener() {
+
+        if (clip != null) {
+            clip.stop();
+            clip.setMicrosecondPosition(0);
+        }
+
+        posicionPausa = 0;
+        pausado = false;
+    }
+
+    public long getPosicionActual() {
+
+        if (clip == null) {
+            return 0;
+        }
+
+        return clip.getMicrosecondPosition();
+    }
+
+    public long getDuracion() {
+
+        if (clip == null) {
+            return 0;
+        }
+
+        return clip.getMicrosecondLength();
+    }
+
+    public boolean estaReproduciendo() {
+
+        return clip != null && clip.isRunning();
+    }
+
+    public boolean estaCargada() {
+        return clip != null;
+    }
+
+    public boolean terminoCancion() {
+
+        if (clip == null) {
+            return false;
+        }
+
+        return clip.getMicrosecondPosition()
+                >= clip.getMicrosecondLength();
+    }
 }
